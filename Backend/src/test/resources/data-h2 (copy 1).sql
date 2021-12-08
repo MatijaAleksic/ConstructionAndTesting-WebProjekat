b47404f68d4f68d6354305f0748ada1fe96a805d
Blizzsuxx <draganarsic1999@hotmail.com>
2021-12-08 15:34:45 +0100

    a

--- Backend/src/test/resources/data-h2 (copy 1).sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO user_table (email,password) VALUES ('markoMarkovic@maildrop.cc','MarkoMarkovic12');
-
-INSERT INTO category (name) VALUES ('Institucija');
-
-INSERT INTO category_type (name,category_id) VALUES ('Muzej', 1);
-

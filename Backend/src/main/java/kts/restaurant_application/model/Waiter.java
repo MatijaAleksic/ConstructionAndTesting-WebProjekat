a10d95864b7731b4b0843acb95de4e726fe517c7
Matija <matijaaleksic97@gmail.com>
2021-12-21 17:39:08 +0100

    uradjen waiter service integration test

--- Backend/src/main/java/kts/restaurant_application/model/Waiter.java
+++ Backend/src/main/java/kts/restaurant_application/model/Waiter.java
@@ -24,7 +24,13 @@ public class Waiter extends User {
 
 	public Waiter(String firstName, String lastName, String username, String password, Date dateOfBirth, Long salary,
 				  boolean deleted,Set<Order> orders) {
+		super(firstName,lastName,username,password,dateOfBirth,salary,deleted);
 		this.orders = orders;
+	}
+
+	public Waiter(String firstName, String lastName, String username, String password, Date dateOfBirth, Long salary,
+				  boolean deleted) {
+		super(firstName,lastName,username,password,dateOfBirth,salary,deleted);
 	}
 
 	public Waiter() {
--- Backend/src/test/java/kts/restaurant_application/services/Integration/ManagerServiceIntegrationTest.java
+++ Backend/src/test/java/kts/restaurant_application/services/Integration/ManagerServiceIntegrationTest.java
@@ -13,16 +13,6 @@ import org.springframework.web.server.ResponseStatusException;
 import static kts.restaurant_application.constants.ManagerConstants.*;
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.server.ResponseStatusException;
-
-import static org.junit.Assert.assertEquals;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestPropertySource("classpath:application-test.properties")

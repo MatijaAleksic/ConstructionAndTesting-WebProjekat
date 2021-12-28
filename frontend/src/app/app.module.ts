124cb4023696a9b449b3482c4a6e15334301c081
nenadicu <urosnenadic18@gmail.com>
2021-12-28 12:04:18 +0100

    Ovo ne radi,baca neke silne greske, nek neko proba da popravi

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CreateCookComponent } from './cook/create-cook/create-cook.component';
+import { UpdateCookComponent } from './cook/update-cook/update-cook.component';
+import { CookTableComponent } from './cook/cook-table/cook-table.component';
+import { CookService } from './service/cook.service';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CreateCookComponent,
+    UpdateCookComponent,
+    CookTableComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+  ],
+  providers: [CookService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

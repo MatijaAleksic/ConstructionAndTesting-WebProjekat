a8a5f4b6bccfde8f798f7a82f0c917d49bedb0e1
nenadicu <urosnenadic18@gmail.com>
2021-12-28 12:23:27 +0100

    Ispravljene sitne greske

--- frontend/src/app/service/cook.service.ts
+++ frontend/src/app/service/cook.service.ts
@@ -3,10 +3,9 @@ import { HttpClient } from '@angular/common/http';
 import {Observable} from "rxjs/index";
 import { ApiResponse } from '../model/api.response';
 import { Cook } from '../model/cook.model';
-import { environment } from 'src/environments/environment';
 
 @Injectable()
-export class EmployeeService {
+export class CookService {
 
   constructor(private http: HttpClient) { }
   private baseUrl: string = 'http://localhost:8080/cooks/';
@@ -21,15 +20,15 @@ export class EmployeeService {
     return this.http.get(this.baseUrl + id);
   }
 
-  createCook(employee: Cook): Observable<ApiResponse> {
+  createCook(cook: Cook): Observable<ApiResponse> {
     return this.http.post<ApiResponse>(this.baseUrl, cook);
   }
 
-  updateCook(id: number, employee: Cook): Observable<ApiResponse> {
+  updateCook(id: number, cook: Cook): Observable<ApiResponse> {
     return this.http.put<ApiResponse>(this.baseUrl + cook.id, cook);
   }
 
-  deleteEmployee(id: number): Observable<ApiResponse> {
+  deleteCook(id: number): Observable<ApiResponse> {
     return this.http.delete<ApiResponse>(this.baseUrl + id);
   }
 }
